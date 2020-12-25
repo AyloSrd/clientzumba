8565dd86b422c78bea7ea5cda3e7abeb2f7d30e6
AyloSrd <aylo.srd@gmail.com>
2020-12-25 19:28:40 +0100

    perfectionned header

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -5,19 +5,25 @@ import './Header.css'
 const Header = () => {
 	return (
 		<div>
-			<header className="Flex Column Header">
-				<img src="assets/base.png" alt="https://unsplash.com/photos/KWrNwBE87EY?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"/>
-				<div id="TitleHeader">
-					<h1 className="WhiteText">Live Interactive<br/>Coding Lessons</h1>
+			<header className="Flex CenteredVHContent Header">
+				<div className="ContentWrapper">
+					<div className="Flex Column JustifyCenterContent">
+						<div id="TitleHeader">
+							<h1 className="WhiteText">Live Interactive<br/>Coding Lessons</h1>
+						</div>
+						<div id="SubtitleHeader">
+							<p id="description">Cöde is a modern school platform for a seamless learning journey. Stream your code and let your students play with it.</p>
+						</div>
+						<NavLink to="/signup">
+							<button className="NeuBtn CTA">
+								Create your first lesson for free!
+							</button>
+						</NavLink>
+					</div>
+					<div class="imgContainer Flex CenteredVHContent">
+						<img src="assets/base.png" alt="https://unsplash.com/photos/KWrNwBE87EY?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"/>
+					</div>
 				</div>
-				<div id="SubtitleHeader">
-					<p id="description">Cöde is a modern school platform for a seamless learning journey. Stream your code and let your students play with it.</p>
-				</div>
-				<NavLink to="/signup">
-					<button className="NeuBtn CTA">
-						Create your first lesson for free!
-					</button>
-				</NavLink>
 			</header>
 		</div>
 	)
